454b4af96aad96e44c88f7384153d4f19895564a
Idris Akkulov <i.akkulov@gmail.com>
2023-12-06 15:33:21 +0300

    db common crud interface

---
+++ internal/handler/user/deps.go
@@ -0,0 +1,13 @@
+package user
+
+import (
+	"github.com/Nahbox/crud-users-api/internal/models"
+)
+
+type DB interface {
+	GetAllUsers() ([]models.User, error)
+	AddUser(user *models.User) error
+	GetUserById(userId int) (models.User, error)
+	UpdateUser(userId int, user *models.User) (models.User, error)
+	DeleteUser(userId int) error
+}
